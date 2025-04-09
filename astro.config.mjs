03d748cf4be8d8327f5c8806dff252ecde724ae7
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-04-09 09:29:04 +0200

    Avancement Header + ajout police Nosifer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-      },
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        ignored: ['backend/pocketbase/**']
+      }
+    }
+  },
+
+  experimental: {
+   svg: true,
+ },
+
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
