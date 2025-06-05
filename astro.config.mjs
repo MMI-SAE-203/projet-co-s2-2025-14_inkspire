9b5fd16ce6a05f0cfbff181600789fdf8c1aaf03
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-06-05 04:16:10 +0200

    Avancemnts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
-
-// import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations:[react()],
   vite: {
     plugins: [tailwindcss()],
@@ -20,6 +21,4 @@ export default defineConfig({
   experimental: {
    svg: true,
  },
-
-  output: 'server',
 });
