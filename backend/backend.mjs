9dbe2116424932c769e2899d7096e02c1b88664e
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2025-06-05 03:43:15 +0200

    Co-authored-by: Victor-KiViK-Landwerlin <Victor-KiViK-Landwerlin@users.noreply.github.com>

---
+++ backend/backend.mjs
@@ -0,0 +1,60 @@
+import PocketBase from 'pocketbase' ;
+const pb = new PocketBase('https://pocketbase.victor-landwerlin.fr') ;
+
+export async function getAllThemes() {
+  try {
+    let records = await pb.collection('theme').getFullList();
+    records = records.map((record) => {
+            record.img = pb.files.getURL(record, record.image);
+            return record;
+        });
+    return records;
+  } catch (error) {
+    console.error('Error fetching themes:', error);
+    throw error;
+  }
+}
+
+export async function getTattoosByTheme(themeId) {
+  try {
+    let records = await pb.collection('tatouage').getFullList({
+      filter: `theme = "${themeId}"`,
+      expand: 'theme'
+    });
+    
+    records = records.map((record) => {
+      record.img = pb.files.getURL(record, record.image);
+      return record;
+    });
+    
+    return records;
+  } catch (error) {
+    console.error('Error fetching tattoos by theme:', error);
+    throw error;
+  }
+}
+
+// Fonction pour récupérer un thème spécifique
+export async function getThemeById(themeId) {
+  try {
+    const record = await pb.collection('theme').getOne(themeId);
+    record.img = pb.files.getURL(record, record.image);
+    return record;
+  } catch (error) {
+    console.error('Error fetching theme:', error);
+    throw error;
+  }
+}
+
+export async function getTattooById(tattooId) {
+  try {
+    const record = await pb.collection('tatouage').getOne(tattooId, {
+      expand: 'theme'
+    });
+    record.img = pb.files.getURL(record, record.image);
+    return record;
+  } catch (error) {
+    console.error('Error fetching tattoo:', error);
+    throw error;
+  }
+}
